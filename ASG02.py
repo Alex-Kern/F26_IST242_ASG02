abe3ae57f6565fb3cc93310096c54d6e65cf0b02
Alexander Kern <apk6489@psu.edu>
2026-09-15 07:37:33 -0400

    git commit -m "feat: add display_menu function"

--- ASG02.py
+++ ASG02.py
@@ -1,2 +1,10 @@
-#test
-print("hello world!")
+def display_menu():
+    print("\n=== Personal Library Manager ===")
+    print("1. View all books")
+    print("2. Add a book")
+    print("3. Remove a book")
+    print("4. Search for a book")
+    print("5. Exit")
+
+
+display_menu()
